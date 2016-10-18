f84883ca488ded26d95a834a6d5441a8130684bb
Matthias Ludwig <matthiasludwig@me.com>
2016-10-18 19:24:04 +0000

    Implemented initials less version

--- initials.c
+++ initials.c
@@ -1,25 +1,20 @@
 #include <stdio.h>
-#include <string.h>
 #include <cs50.h>
+#include <string.h>
 #include <ctype.h>
 
-int main (int argc, char* argv[])
-{
-    string name = GetString();
+int main(void) {
+    string name = get_string();
     
-    if (name != NULL) // check if the input is valid
-    {
-    
-    printf("%c", toupper(name[0])); // Print the first char upperspace
-    
-        for (int i = 0, j = strlen(name); i < j; i++) // Go through all chars
-            {
-                if (name[i] == ' ')                   // And search for any spaces
-                    printf("%c", toupper(name[i+1])); // Take the character after the space and print it uppercase
+    if (name != NULL) {
+        printf("%c", toupper(name[0]));
+        for (int i = 0, j = strlen(name); i < j; i++) {
+            if (name[i] == ' ') {
+                printf("%c", toupper(name[i + 1]));
             }
-            printf("\n");
+        }
+        printf("\n");
+        return 0;
     }
-    
-    
-    return 0;
+    return 1;
 }
