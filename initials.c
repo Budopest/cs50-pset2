a430c7d70367c030f01ab04fd5ff9aae9dd085ba
Matthias Ludwig <matthiasludwig@me.com>
2016-10-18 20:55:53 +0000

    Added initials more comfortable

--- initials.c
+++ initials.c
@@ -7,10 +7,15 @@ int main(void) {
     string name = get_string();
     
     if (name != NULL) {
-        printf("%c", toupper(name[0]));
+        // Test the first letter if it is a space or not
+        if (name[0] != ' ') {
+            printf("%c", toupper(name[0]));
+            
+        }
+        //Loop through the rest of the name and check if there are spaces
         for (int i = 0, j = strlen(name); i < j; i++) {
-            if (name[i] == ' ') {
-                printf("%c", toupper(name[i + 1]));
+            if (name[i] == ' ' && name[i+1] != ' ' && name[i+1] != '\0') {
+                printf("%c", toupper(name[i + 1]));   
             }
         }
         printf("\n");
